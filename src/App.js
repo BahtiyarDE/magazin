3229cfaa242d5674fdd4ad71e24fef1b22daf3a3
BahtiyarDE <rahmnv1b@gmail.com>
2022-05-10 15:10:34 +0600

    deb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import {Routes, Route} from "react-router-dom"
+import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import Products from "./components/Products";
+import Basket from "./components/Basket";
+import Wishlist from "./components/Wishlist";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="md:container">
+            <Header/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contacts" element={<Contacts/>}/>
+                <Route path="/products" element={<Products/>}/>
+                <Route path="/wishlist" element={<Wishlist/>}/>
+                <Route path="/basket" element={<Basket/>}/>
+            </Routes>
+
+
+        </div>
+    );
 }
 
 export default App;
